bcadda96445841a97c218acafc04a2b73e964013
grzlz <guillermo.rdzlpz@gmail.com>
2023-08-11 06:08:37 -0600

    upload function.zip to bucket and make lambda query data from s3 instead from asset

--- backend/photo_validator/photo_validator_stack.py
+++ backend/photo_validator/photo_validator_stack.py
@@ -3,7 +3,8 @@ from aws_cdk import (
     Stack,
     aws_lambda as _lambda,
     aws_apigateway as apigw,
-    aws_iam as iam
+    aws_iam as iam,
+    aws_s3 as s3
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -13,6 +14,8 @@ class PhotoValidatorStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        s3_bucket = s3.Bucket(self, 'icarus-lambda')
+
         api = apigw.RestApi(self, 'ImageTagAPI',
             rest_api_name='Image tagger',
             description='This service tags images.'
@@ -21,9 +24,15 @@ class PhotoValidatorStack(Stack):
         tag_lambda = _lambda.Function(
             self, 'TagLambda',
             runtime=_lambda.Runtime.PYTHON_3_7,
-            code=_lambda.Code.from_asset("lambda/function.zip"),
-            handler='image_tagger.handler'
+            code=_lambda.Code.from_bucket(s3_bucket, 'function.zip'),
+            handler='image_tagger.handler',
+            environment={
+                'S3_BUCKET': 'icarus-lambda',
+                'ZIP_FILE_KEY': 'function.zip'
+            }
         )
+
+        s3_bucket.grant_read(tag_lambda)
 
         tag_lambda.add_to_role_policy(
             iam.PolicyStatement(
